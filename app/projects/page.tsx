c32d60f0a5c8d3712f285068fc9d03cf9325989e
Hamdi Moubrak <104172250+ElCapor@users.noreply.github.com>
2024-04-21 12:38:08 +0200

    Create page.tsx

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+      <section>
+        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+          My projects
+        </h1>
+        <a> Soon my projects here</a>
+      </section>
+    )
+  }
