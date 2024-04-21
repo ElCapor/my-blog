6ce5f0ed7a7b85fbe6ce8a55a903d6f2e14e69e7
Hamdi Moubrak <104172250+ElCapor@users.noreply.github.com>
2024-04-21 12:37:09 +0200

    Customization

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'ElCapor\'s blog',
+    template: '%s | ElCapor\'s blog',
   },
   description: 'This is my portfolio.',
   openGraph: {
